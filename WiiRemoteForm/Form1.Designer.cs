060427616f51c5dd7e2223c7869af2fd9032176f
Ichinose Shugo <ichinose@srmtlab.org>
2017-01-16 18:36:59 +0900

    wiiリモコンの加速度書き込み

--- WiiRemoteForm/Form1.Designer.cs
+++ WiiRemoteForm/Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.wiiRec = new System.Windows.Forms.CheckBox();
+            this.ax = new System.Windows.Forms.Label();
+            this.ay = new System.Windows.Forms.Label();
+            this.az = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // wiiRec
+            // 
+            this.wiiRec.AutoSize = true;
+            this.wiiRec.Location = new System.Drawing.Point(12, 25);
+            this.wiiRec.Name = "wiiRec";
+            this.wiiRec.Size = new System.Drawing.Size(39, 16);
+            this.wiiRec.TabIndex = 1;
+            this.wiiRec.Text = "Wii";
+            this.wiiRec.UseVisualStyleBackColor = true;
+            // 
+            // ax
+            // 
+            this.ax.AutoSize = true;
+            this.ax.Location = new System.Drawing.Point(12, 44);
+            this.ax.Name = "ax";
+            this.ax.Size = new System.Drawing.Size(25, 12);
+            this.ax.TabIndex = 2;
+            this.ax.Text = "x軸:";
+            // 
+            // ay
+            // 
+            this.ay.AutoSize = true;
+            this.ay.Location = new System.Drawing.Point(12, 60);
+            this.ay.Name = "ay";
+            this.ay.Size = new System.Drawing.Size(25, 12);
+            this.ay.TabIndex = 3;
+            this.ay.Text = "y軸:";
+            // 
+            // az
+            // 
+            this.az.AutoSize = true;
+            this.az.Location = new System.Drawing.Point(12, 76);
+            this.az.Name = "az";
+            this.az.Size = new System.Drawing.Size(24, 12);
+            this.az.TabIndex = 4;
+            this.az.Text = "z軸:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.az);
+            this.Controls.Add(this.ay);
+            this.Controls.Add(this.ax);
+            this.Controls.Add(this.wiiRec);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.CheckBox wiiRec;
+        private System.Windows.Forms.Label ax;
+        private System.Windows.Forms.Label ay;
+        private System.Windows.Forms.Label az;
     }
 }
 
--- WiiRemoteForm/Form1.cs
+++ WiiRemoteForm/Form1.cs
@@ -3,18 +3,75 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WiimoteLib;
 
 namespace WiiRemoteForm
 {
     public partial class Form1 : Form
     {
+        Wiimote wm = new Wiimote();
+        static public DateTime dt = DateTime.Now;
+        static string pathDoc = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        StreamWriter sw;
+        int frame = 0;
+
         public Form1()
         {
+            Control.CheckForIllegalCrossThreadCalls = false;
             InitializeComponent();
+            Init();
+            wm.WiimoteChanged += wm_WiimoteChanged;
+            wm.SetReportType(InputReport.ButtonsAccel, true);
+            wm.Connect();
+        }
+        public void Init()
+        {
+            //ファイル書き込み用のdirectoryを用意
+            string date = dt.Year+digits(dt.Month)+digits(dt.Day)+digits(dt.Hour)+digits(dt.Minute);
+            string path = pathDoc + "/HoeData/" + date;
+            Directory.CreateDirectory(path);
+            //ファイルを用意
+            sw = new StreamWriter(path + "/Wii.csv", false);
+        }
+
+        private void wm_WiimoteChanged(object sender, WiimoteChangedEventArgs e)
+        {
+            WiimoteState ws = e.WiimoteState;
+            WriteFile(ws);
+            DrawForms(ws);
+            frame++;
+        }
+
+        public void WriteFile(WiimoteState ws)
+        {
+            if (wiiRec.Checked)
+                sw.WriteLine(frame+","+ws.AccelState.Values.X+","+ ws.AccelState.Values.Y+","+ ws.AccelState.Values.Z);
+        }
+
+        public void DrawForms(WiimoteState ws)
+        {
+            if (wiiRec.Checked)
+            {
+                ax.Text = "x軸:" + ws.AccelState.Values.X;
+                ay.Text = "y軸:" + ws.AccelState.Values.Y;
+                az.Text = "z軸:" + ws.AccelState.Values.Z;
+            }
+        }
+
+        public String digits(int date)
+        {
+            if (date / 10 == 0) return "0" + date;
+            else return date.ToString();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            sw.Close();
         }
     }
 }
